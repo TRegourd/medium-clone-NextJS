be180077315acfef989376a783809c3257e1cc9b
Thomas Regourd <tom.reg@hotmail.fr>
2022-06-03 12:36:19 +0200

    add posts to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[];
+}
+
+const Home = ({ posts }: Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -14,8 +19,56 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+      {/*Posts*/}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 lg:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+              ></img>
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    slug,
+    author ->{
+    name, image
+  },
+  description,
+  mainImage,
+  slug
+  }
+  `;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: { posts },
+  };
+};
